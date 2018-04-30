535b0779ae7e0a1772af67ae56f029749c603a20
Jinma Yamashita <jinmayamashita@gmail.com>
2018-05-01 01:02:06 +0900

    Update example

--- examples/App.vue
+++ examples/App.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="app">
-    <vue-aiie-contenteditor withTitle />
+    <vue-aiie-contenteditor
+      :dispatch=bindEditorValue
+      :contentTitle="value.title"
+    />
+    <pre class="console">{{ JSON.stringify(value, null, '\t') }}</pre>
   </div>
 </template>
-<style>
-.example {
-  border: 1px solid #999;
+<script>
+
+export default {
+  data() {
+    return {
+      value: {
+        title: "",
+        content: ""
+      },
+    };
+  },
+  methods: {
+    bindEditorValue(editorValue) {
+      this.value = {
+        title: editorValue.title,
+        content: editorValue.content
+      }
+    }
+  }
 }
-.example .ProseMirror {
-  min-height: 300px;
+</script>
+
+<style>
+.console {
+  width: 800px;
+  height: 100px;
+  overflow-y: scroll;
+  margin: 30px auto;
+  padding: 20px;
+  box-sizing: border-box;
+  background: #000;
+  color: #eee;
+  font-family: menlo;
+  font-size: 11px;
+  border-radius: 4px;
 }
 </style>
