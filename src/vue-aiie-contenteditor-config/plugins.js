8186602d12ce0563f8d889b227edf1459f44536e
Jinma Yamashita <jinmayamashita@gmail.com>
2018-05-01 01:02:52 +0900

    Placeholder to Prosemirror plugin

--- src/vue-aiie-contenteditor-config/plugins.js
+++ src/vue-aiie-contenteditor-config/plugins.js
@@ -1,5 +1,6 @@
 import { history } from "prosemirror-history";
+import placeholder from "../prosemirror-placeholder";
 import keys from "./keys";
 import rules from "./rules";
 
-export default [rules, keys, history()];
+export default [rules, keys, history(), placeholder()];
